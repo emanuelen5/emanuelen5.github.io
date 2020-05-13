699ab1ec14b1356b483fad87f22fea66b61d3e23
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2020-05-13 21:45:43 +0100

    Generating tags in Liquid instead of js

--- site/assets/js/posts.js
+++ site/assets/js/posts.js
@@ -15,26 +15,3 @@ let cards = {
 	]},
 {%- endfor -%}
 };
-
-for (post in cards) {
-	const c = cards[post];
-	const card = document.getElementById(post);
-	const tags = card.querySelector(".tags");
-	for (const i in c.tags) {
-		let tag = document.createElement("span");
-		let tc = tag.classList;
-		tc.add("badge");
-		tc.add("badge-dark");
-		tag.innerHTML = c.tags[i];
-		tags.appendChild(tag);
-	}
-	const categories = card.querySelector(".categories");
-	for (const i in c.categories) {
-		let cat = document.createElement("span");
-		let tc = cat.classList;
-		tc.add("badge");
-		tc.add("badge-info");
-		cat.innerHTML = c.categories[i];
-		categories.appendChild(cat);
-	}
-}
