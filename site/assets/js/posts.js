e0a0ae43c808fe4552a66b04e097ab1cb7eb19ce
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2020-05-21 21:05:54 +0100

    Filtering on both tag and category

--- site/assets/js/posts.js
+++ site/assets/js/posts.js
@@ -21,25 +21,25 @@ TYPE_CATEGORY = "categories";
 
 function set_visibility(elem, visible) {
 	if (visible) {
+		console.log(elem.id + " as visible");
 		elem.style.removeProperty("height");
 		elem.style.removeProperty("visibility");
 	} else {
+		console.log(elem.id + " as hidden");
 		elem.style.height = 0;
 		elem.style.visibility = "hidden";
 	}
 }
 
-function update_visibility(type, name, filters) {
+let tag_filters = [], category_filters = [];
+
+function update_visibility() {
 	for (let cardname in cards) {
 		const card = cards[cardname];
 		const elem = document.getElementById(cardname);
-		if (filters.length == 0) {
-			console.log(cardname + " as visible");
-			set_visibility(elem, true);
-		} else {
-			console.log(cardname + " as hidden");
-			set_visibility(elem, card[type].indexOf(name) >= 0);
-		}
+		set_visibility(elem,
+			tag_filters.every((elem) => card[TYPE_TAG].indexOf(elem) >= 0) &&
+			category_filters.every((elem) => card[TYPE_CATEGORY].indexOf(elem) >= 0));
 	};
 }
 
@@ -51,10 +51,8 @@ function filter(type, name, filters) {
 		filters.push(name);
 	}
 	console.log(type + " clicked!: " + name + ". Filters active: " + filters);
-	update_visibility(type, name, filters);
+	update_visibility();
 };
-
-let tag_filters = [], category_filters = [];
 
 function create_filter_callback(type, name, filters) {
 	return (event) => {filter(type, name, filters)};
